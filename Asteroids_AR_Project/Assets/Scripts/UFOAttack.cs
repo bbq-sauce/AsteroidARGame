a1a16f0d82556236584a43c45f26d0bfb5843c24
Fiza <fizasameer88@gmail.com>
2023-10-27 14:48:48 +0530

    completed till setting up UFO Attacks;
    6 more readings left

---
+++ Asteroids_AR_Project/Assets/Scripts/UFOAttack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UFOAttack : MonoBehaviour
+{
+
+    [SerializeField] private float _fireCooldownTime = 0.3f;
+    [SerializeField] private UnityEvent OnShoot;
+    // Start is called before the first frame update
+
+    
+    private void OnEnable()
+    {
+        StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        while (enabled)
+        {
+            OnShoot?.Invoke();
+            yield return new WaitForSeconds(_fireCooldownTime);
+        }
+    }
+}
